21298a3b72e0159df2ffd2cd3fcca8e3f6ce01d6
JoiceJolly <30997045+JoiceJolly@users.noreply.github.com>
2017-09-01 15:07:45 +0530

    Add files via upload

---
+++ QueueUpYourMessages/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace QueueUpYourMessages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtQueue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnMessages = new System.Windows.Forms.Button();
+            this.lstMessage = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Azure Queue Name";
+            // 
+            // txtQueue
+            // 
+            this.txtQueue.Location = new System.Drawing.Point(166, 71);
+            this.txtQueue.Name = "txtQueue";
+            this.txtQueue.Size = new System.Drawing.Size(100, 20);
+            this.txtQueue.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Messages";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(166, 109);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(100, 20);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(98, 174);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add Messages";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(345, 47);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(103, 23);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "Click To Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnMessages
+            // 
+            this.btnMessages.Location = new System.Drawing.Point(454, 47);
+            this.btnMessages.Name = "btnMessages";
+            this.btnMessages.Size = new System.Drawing.Size(100, 23);
+            this.btnMessages.TabIndex = 6;
+            this.btnMessages.Text = "List Messages";
+            this.btnMessages.UseVisualStyleBackColor = true;
+            this.btnMessages.Click += new System.EventHandler(this.btnMessages_Click);
+            // 
+            // lstMessage
+            // 
+            this.lstMessage.FormattingEnabled = true;
+            this.lstMessage.Location = new System.Drawing.Point(345, 89);
+            this.lstMessage.Name = "lstMessage";
+            this.lstMessage.Size = new System.Drawing.Size(209, 121);
+            this.lstMessage.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 299);
+            this.Controls.Add(this.lstMessage);
+            this.Controls.Add(this.btnMessages);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtQueue);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQueue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnMessages;
+        private System.Windows.Forms.ListBox lstMessage;
+    }
+}
+
---
+++ QueueUpYourMessages/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Auth;
+
+namespace QueueUpYourMessages
+{
+    public partial class Form1 : Form
+    {
+        //string storageAccountName = "augappdiag253";
+        //string accountAccountKey = "aArrnNVENF5D/9oK3R5FfAWCINp2+eenDpdIkxOje/SJy0i4gr8KnYySuBKsEcER7W5jwUpXsG8eTwve/7Yn5w==";
+        CloudStorageAccount account = new CloudStorageAccount(new StorageCredentials("augappdiag253","6w6ranWlhfOyWrorrZIUGLQ6KmECvJZNIEkVSpVS9A0jIW9FOw7EPvNq8pSk1n3gMrFaz3O2KeJJs4lbSmyahQ=="), useHttps: true);
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            CloudQueueClient cloudQueueClient = account.CreateCloudQueueClient();
+            string queueName = txtQueue.Text;
+            CloudQueue azureQueue = cloudQueueClient.GetQueueReference(queueName);
+            azureQueue.CreateIfNotExists();
+            MessageBox.Show("Azure Queue created");
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            CloudQueueClient cloudQueueClient = account.CreateCloudQueueClient();
+            string message = txtMessage.Text;
+            CloudQueueMessage cloudQueueMessage = new CloudQueueMessage(message);
+            CloudQueue azureQueue = cloudQueueClient.GetQueueReference("queue1");
+            azureQueue.AddMessage(cloudQueueMessage);
+            txtMessage.Text = "";
+        }
+
+        private void btnMessages_Click(object sender, EventArgs e)
+        {
+            CloudQueueClient cloudQueueClient = account.CreateCloudQueueClient();
+            CloudQueue azureQueue = cloudQueueClient.GetQueueReference("queue1");
+            var messages = azureQueue.GetMessages(10);
+            foreach (var message in messages)
+            {
+                lstMessage.Items.Add(message.AsString);
+            }
+        }
+    }
+    
+}
